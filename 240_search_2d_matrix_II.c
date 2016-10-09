4e184a8201d8b732e32edfda6a913116d024bb51
wuzhouhui <wuzhouhui250@gmail.com>
2016-10-09 22:33:41 +0800

    add 240

---
+++ 240_search_2d_matrix_II.c
@@ -0,0 +1,19 @@
+#ifndef bool
+#define bool int
+#endif
+
+bool searchMatrix(int **matrix, int matrixRowSize, int matrixColSize,
+		int target)
+{
+	int r = 0, c = matrixColSize - 1;
+	while (r < matrixRowSize && c >= 0) {
+		if (matrix[r][c] == target)
+			return(1);
+		else if (matrix[r][c] < target)
+			r++;
+		else
+			c--;
+	}
+	return(0);
+}
+main(){}
