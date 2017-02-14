9cb71dbd5560ceffb41b348bd7f39eb9e1228bd2
wuzhouhui <wuzhouhui250@gmail.com>
2017-02-14 22:25:46 +0800

    Add 442 and 448

---
+++ 448_find_disppeared.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+static int p, maxp, *res;
+
+static void add(int a)
+{
+	if (p >= maxp) {
+		maxp += 8;
+		res = realloc(res, maxp * sizeof(*res));
+	}
+	res[p++] = a;
+}
+
+static void swap(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int *findDisappearedNumbers(int *nums, int numsSize, int *returnSize)
+{
+	if (numsSize <= 0) {
+		*returnSize = 0;
+		return(NULL);
+	}
+
+	int i;
+
+	res = NULL;
+	maxp = p = 0;
+	for (i = 0; i < numsSize; i++) {
+		if (nums[i] == i + 1)
+			continue;
+		while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i])
+			swap(&nums[i], &nums[nums[i] - 1]);
+	}
+	for (i = 0; i < numsSize; i++)
+		if (nums[i] != i + 1)
+			add(i + 1);
+	*returnSize = p;
+	return(res);
+}
+
+int main(void)
+{
+}
